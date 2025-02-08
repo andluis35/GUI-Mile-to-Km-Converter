1ce9fa24fc3a756f0bec6a12b43af71d2527b0df
Anderson Luis <and.luis35@gmail.com>
2025-02-08 19:59:28 -0300

    Teste de componentes

--- Converter/main.py
+++ Converter/main.py
@@ -1,6 +1,19 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+def button_clicked():
+    label["text"] = input_box.get()
+
+window = Tk()
 window.minsize(width=500, height=500)
+
+label = Label()
+label["text"] = "Texto"
+label.pack()
+
+button = Button(text="Click me!", command=button_clicked)
+button.pack()
+
+input_box = Entry(width=10)
+input_box.pack()
 
 window.mainloop()
