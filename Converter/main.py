8b2725a2ba37a3903fb25968aa3531bb5f81e40b
Anderson Luis <and.luis35@gmail.com>
2025-02-12 11:17:04 -0300

    Adicionada função de conversão

--- Converter/main.py
+++ Converter/main.py
@@ -1,5 +1,10 @@
 from tkinter import *
 
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    kilometer_result_label.config(text=f"{km}")
+
 window = Tk()
 window.minsize(width=150,height=100)
 window.config(padx=20, pady=20)
@@ -20,7 +25,7 @@ kilometer_result_label.grid(column=1, row=1)
 kilometer_label = Label(text="Km")
 kilometer_label.grid(column=2, row=1)
 
-calculate_button = Button(text="Calculate")
+calculate_button = Button(text="Calculate", command=miles_to_km)
 calculate_button.grid(column=1, row=2)
 
 
